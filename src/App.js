e7f767e75e52e4225514d46c4f356eb768c60a31
dos <was607412@gmail.com>
2023-09-06 15:38:43 +0900

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>영화와 드라마</p>
+    </div>
+  );
+}
+
+export default App;
