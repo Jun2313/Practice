6f2acdc1b43762c8b5749d6904a0be1b67e08e5e
3-yellow-1 <3.yellow.1@gmail.com>
2023-09-07 09:22:23 +0900

    파일 구성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Movie from './screens/Movie';
+import Actor from './screens/Actor';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
     <div className="App">
-      <p>영화와 드라마</p>
-      <p>테스트</p>
-      <p>확인용</p>
-
+      <Routes>
+        <Route path="/" element={ <Movie /> } />
+        <Route path="/Actor" element={ <Actor /> } />
+        <Route path="/NavBar" element={ <NavBar /> } />
+      </Routes>
     </div>
   );
 }
---
+++ src/screens/Movie.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+
+function Movie() {
+
+    return (
+      <div className="Movie">
+        <NavBar />
+        영화 검색 화면입니다.
+      </div>
+    );
+  }
+  
+  export default Movie;
