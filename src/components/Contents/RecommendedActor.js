f39efa80fcb17711ec6f5302badc167e28dc7558
3-yellow-1 <3.yellow.1@gmail.com>
2023-09-11 11:36:58 +0900

    [style]RecommendedActor

--- src/components/Contents/RecommendedActor.js
+++ src/components/Contents/RecommendedActor.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';   
+
 function RecommendedActor() {
     return (
         <div className="RecommendedActor">
-            추천배우 리스트
+            <Carousel className='Carousel'>
+                    <Carousel.Item interval={2000} className='Carousel.Item'>
+                        <h3>김태희</h3>
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000} className='Carousel.Item'>
+                        <h3>송혜교</h3>            
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000} className='Carousel.Item'>
+                        <h3>전지현</h3>            
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000} className='Carousel.Item'>
+                        <h3>박보영</h3>              
+                    </Carousel.Item>
+                    <Carousel.Item interval={2000} className='Carousel.Item'>
+                        <h3>전도연</h3> 
+                    </Carousel.Item>
+                </Carousel>
         </div>
     );
 }
